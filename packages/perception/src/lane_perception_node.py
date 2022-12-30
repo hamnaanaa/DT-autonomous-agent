37842d9acb9925bbf4d7022193663a8af66e5e5c
Hamudi Naanaa <hamudi.naanaa@tum.de>
2022-12-30 18:11:15 +0100

    Integrate segmentation CNN into the perception pipeline + publish the prediction to a new rostopic

--- packages/perception/src/lane_perception_node.py
+++ packages/perception/src/lane_perception_node.py
@@ -35,9 +35,9 @@ class LanePerceptionNode(DTROS):
                                                  node_type=NodeType.BEHAVIOR)
         self.veh_name = rospy.get_namespace().strip("/")
 
-        self.bridge = CvBridge()
+        # self.bridge = CvBridge()
 
-        self.camera_config = CameraConfig.CameraConfig.from_file(f"/data/config/calibrations/camera_intrinsic/{self.veh_name}.yaml")
+        # self.camera_config = CameraConfig.CameraConfig.from_file(f"/data/config/calibrations/camera_intrinsic/{self.veh_name}.yaml")
 
         # params
         rospy.set_param(f"/{self.veh_name}/lane_perception_node/gamma", 0.9)
@@ -50,22 +50,22 @@ class LanePerceptionNode(DTROS):
         self.mode_sub = rospy.Subscriber(f"/{self.veh_name}/state_machine_node/mode", FSMState, self.cb_on_mode_change)
         self.im_sub = rospy.Subscriber(f"/{self.veh_name}/camera_node/image/compressed", CompressedImage, self.cb_img, queue_size=1,
                                        buff_size=1000000)
-        self.lane_det_pub = rospy.Publisher(f"/{self.veh_name}/lane_perception_node/lane_det", LaneDetection, queue_size=1)
+        # self.lane_det_pub = rospy.Publisher(f"/{self.veh_name}/lane_perception_node/lane_det", LaneDetection, queue_size=1)
         self.line_pub = rospy.Publisher(f"/{self.veh_name}/lane_perception_node/lines/compressed", CompressedImage, queue_size=1)
         # self.red_line_pub = rospy.Publisher(f"/{self.veh_name}/lane_perception_node/red_lines/compressed", CompressedImage, queue_size=1)
 
         change_mode_srv = f"/{self.veh_name}/state_machine_node/change_mode"
         self.mode_srv = rospy.ServiceProxy(change_mode_srv, SetFSMState)
 
-        self.last_red = rospy.get_time()
-        self.red_active = False
+        # self.last_red = rospy.get_time()
+        # self.red_active = False
 
-        self.rect_mat = None
+        # self.rect_mat = None
 
         self.loginfo("Initialized")
         
-        self.loginfo(f"Use pytorch of version: {torch.__version__}")
-        self.loginfo(f"Use torchvision of version: {torchvision.__version__}")
+        # self.loginfo(f"Use pytorch of version: {torch.__version__}")
+        # self.loginfo(f"Use torchvision of version: {torchvision.__version__}")
         
         hparams = {
             # --- Model ---
@@ -89,45 +89,57 @@ class LanePerceptionNode(DTROS):
         self.model.load_state_dict(torch.load(path_to_model))
         self.loginfo("[HAM] Loaded model with state dict!")
         
+        self.avg_process_time = 0
+        
 
     def cb_on_mode_change(self, msg):
         if msg.state == "LANE_FOLLOWING":
             self.red_active = True
 
+    def _pred_to_img(self, label_img, color_map):
+        # Convert the label image to a color image
+        label_img = label_img.cpu().numpy()
+        color_img = np.zeros((label_img.shape[0], label_img.shape[1], 3), dtype=np.uint8)
+        for label, color in color_map.items():
+            color_img[label_img == label] = color
+        
+        # Convert the color image to a PIL image
+        color_img = Image.fromarray(color_img)
+        return color_img
+        
+
 
     def cb_img(self, message):
         # if self.camera_config is None or self.camera_header is None:
         #    return
-        if not self.model:
+        if not hasattr(self, "model"):
             self.loginfo("[HAM] Model is not initialized yet!")
             return
         
+        time = rospy.Time.now()
+        # self.loginfo(f"[HAM] Received image at time: {time}")
         # Convert the image to a PIL image and then back to bytes to test the conversion
         image = Image.open(io.BytesIO(message.data)).convert("RGB")
-        self.loginfo(f"[HAM] Image received and converted to PIL Image with size: {image.size}")
-        
-        # RAW back -> And then back to bytes
-        # buf = io.BytesIO()
-        # image.save(buf, format="JPEG")
+        # self.loginfo(f"[HAM] Image received and converted to PIL Image with size: {image.size}")
 
-        
-        
         image = transforms.ToTensor()(image)
         # self.loginfo(f"[HAM] Tensor image size: {image.size()}")
         
-        # result = torch.argmax(self.model(image.unsqueeze(0)), dim=1)[0]
-        # self.loginfo(f"[HAM] Result size: {result.size()}. Create a CompressedImage...")
+        image = torch.argmax(self.model(image.unsqueeze(0)), dim=1)[0]
+        image = self._pred_to_img(image, {0: [0, 0, 0], 1: [0, 255, 0]})
+        # self.loginfo(f"[HAM] Prediction size: {image.shape}. Create a CompressedImage from it. Here are the first 10 values: {image[:10]}")
         
         # Convert the result to a CompressedImage to publish on ROS topic
         msg = CompressedImage()
         msg.header.stamp = rospy.Time.now()
         msg.format = "jpeg"
         buf = io.BytesIO()
-        transforms.ToPILImage()(image).save(buf, format="JPEG")
+        image.save(buf, format="JPEG")
         msg.data = buf.getvalue()
         
-        
-        self.loginfo(f"[HAM] Created a message to publish.")
+        process_time = (rospy.Time.now() - time).to_sec()
+        self.avg_process_time = process_time if self.avg_process_time == 0 else (self.avg_process_time * 0.75 + process_time * 0.25)
+        self.loginfo(f"[HAM] Created a message to publish. Process time (curr/avg): {process_time}/{self.avg_process_time}")
         self.line_pub.publish(msg)
         
         
